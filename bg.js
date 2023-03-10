76f987e8472306f0a12acc3c0cfb42681f0d4898
europa_fantasy <92041098+EuropaFantasy@users.noreply.github.com>
2023-03-10 11:49:47 +0100

    Update bg.js

--- bg.js
+++ bg.js
@@ -7,3 +7,37 @@ document.addEventListener("mousemove", function(event) {
     bg.style.marginTop = "calc("+yPos+"px - 205%)";
     
 });
+
+const windowHeight = window.innerHeight;
+
+const halfWindowHeight = windowHeight / 2;
+
+// Fonction de redirection vers une autre page
+
+function redirect() {
+
+  window.location.replace("http://www.example.com");
+
+}
+
+// Écouter l'événement de défilement
+
+window.addEventListener("scroll", () => {
+
+  // Récupérer la position de défilement actuelle
+
+  const scrollPosition = window.scrollY;
+
+  
+
+  // Vérifier si la position de défilement est supérieure à la moitié de la hauteur de la fenêtre
+
+  if (scrollPosition > halfWindowHeight) {
+
+    // Si oui, rediriger vers la page spécifiée
+
+    redirect();
+
+  }
+
+});
